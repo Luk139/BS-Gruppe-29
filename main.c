b66ff31cc6d912e92475f80adf9b145d55d7fc64
Luk139 <feydrummer130@gmail.com>
2022-05-13 09:26:47 +0200

    Exit Funktion drin, ansonsten mental boom

--- main.c
+++ main.c
@@ -11,12 +11,21 @@
 //Port zum verbinden mit Docker Container über Telnet mit telnet 127.0.0.1 5688 (oder welcher Port hier definiert wird)
 #define PORT_NUMBER 5691
 
-int main() {
+int put(char* key, char* value){
 
+}
+int get(char* key, char* res){
+
+}
+int del(char* key){
+
+}
+
+
+int main() {
     //File-Descriptor Rendezvous und Connect
     int rndvz_fd;
     int cnnct_fd;
-
     //  Struct für die Verbindung zwischen client und server
     struct sockaddr_in client;
     //Länge der Client-Data
@@ -25,24 +34,15 @@ int main() {
     int read_bytes;
     // Char-Array für Nachrichten vom Client zum Server
     char in[BUFSIZE];
-
-    /*  Creating the socket with ipv4 and tcp no further description for protocol needed so 0 is enough -> SOCK_STREAM uses tcp by default.
-*   AF_INET = ipv4
-*   SOCK_STREAM = tcp
-*   protocol 0 = default (For other protocols to expect when working with ip)
-*/
     rndvz_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    //  Return value of rndzvs_fd is negative when an error occurs. And 0 when successfull. If it wasn't successfull terminate program
+    //  Return value von rndzvs_fd ist negative bei error und 0 when erfolgreich. Falls nicht erfolgreich - Terminiert sich das Programm
     if (rndvz_fd < 0) {
         perror("Error while socket()");
         return EXIT_FAILURE;
     }
-
     // Set Option for the Socket for rapid iterative binding to address
     int option = 1;
     setsockopt(rndvz_fd, SOL_SOCKET, SO_REUSEADDR, (const void *) &option, sizeof(int));
-
     //  Struct for the connection between client and server
     struct sockaddr_in server;
     //  Define to use ipv4
@@ -51,67 +51,50 @@ int main() {
     server.sin_addr.s_addr = INADDR_ANY;
     //  Define port number on which the client wants to connect
     server.sin_port = htons(PORT_NUMBER);
-
-    /*  Bind an address to the socket
-     *  rndzvs_fd = socket file descriptor
-     *  addr = address struct for cast
-     *  len = length of the address
-     */
     int bnd_result = bind(rndvz_fd, (struct sockaddr *) &server, sizeof(server));
     // Ask for return value of the function to check for error while binding => terminate program if error
     if (bnd_result < 0) {
         perror("Error while bind()");
         return EXIT_FAILURE;
     }
-
     //  Max. capacity for clients communicating with the server
     const int max_clients = 5;
-
-    /*  Bring socket to listen for incoming connections
-     *  rndzvs_fd = socket file descriptor
-     *  n = maximum number of clients
-     *
-     */
     int lstn_result = listen(rndvz_fd, max_clients);
     // Check return value for error
     if (lstn_result < 0) {
         perror("Error while listen()");
         return EXIT_FAILURE;
     }
-
-
     while (LOOP) {
-
-        /*  Accept incoming connection request of the client
-        *
-        *  cnnct_fd = socket file descriptor
-        *  addr = source adress
-        *  addr_len = size of source adress
-        */
         cnnct_fd = accept(rndvz_fd, (struct sockaddr *) &client, &client_len);
         if (cnnct_fd < 0) {
             perror("Error while accept()");
             return EXIT_FAILURE;
         }
-
-
         // read the bytes from the client
         read_bytes = read(cnnct_fd, in, BUFSIZE);
         if (read_bytes < 0) {
             perror("Error while read()");
             return EXIT_FAILURE;
         }
-        // Sending back data as long the client keeps sending some
+        //Das while sendet solange etwas zurück, wie der client was schreibt
         while (read_bytes > 0) {
+            //Clion console ausgabe, client terminal sieht hiervon nichts
             printf("sending back the %d bytes I received...\n", read_bytes);
 
             write(cnnct_fd, in, read_bytes);
 
             read_bytes = read(cnnct_fd, in, BUFSIZE);
-
+            if (strncmp("exit", in,4) == 0) {
+                printf("Server Exit...\n");
+                break;
+            }
+            //FUnktioniert NICHT
+            if(strncmp("put",in,BUFSIZE)==0){
+                write(cnnct_fd,"okey test funktioniert",read_bytes);
+            }
         }
         close(cnnct_fd);
     }
-
     close(read_bytes);
 }

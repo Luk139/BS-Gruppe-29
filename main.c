06fc3bff8a50b7a07680dd542332ab6fe0b223ab
Tobias Mai <tmai@ssd-consulting.de>
2022-05-13 13:37:34 +0200

    Iterativer Server fortified

--- main.c
+++ main.c
@@ -1,86 +1,164 @@
-/*******************************************************************************
-
-  Ein TCP-Echo-Server als iterativer Server: Der Server schickt einfach die
-  Daten, die der Client schickt, an den Client zurück.
-
-*******************************************************************************/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <string.h>
 
-#define BUFSIZE 1024 // Größe des Buffers
-#define TRUE 1
-#define ENDLOSSCHLEIFE 1
-#define PORT 5678
+#define LOOP 1
+#define LENGTH 127
+//  Look in "Docker Desktop" for opened ports on container
+#define PORT_NUMBER 5678
 
+int main(){
 
-int main() {
+    //  Struct for the connection between client and server
+    struct sockaddr_in address;
 
-    int rfd; // Rendevouz-Descriptor
-    int cfd; // Verbindungs-Descriptor
+    //  Integer for length of the address
+    int addr_length = sizeof(address);
+    //  Filedescriptor(Rendezvous-Descriptor), integer for identifying the socket. Set to -1 so we dont get a "success" value by default.
+    int rndzvs_fd = -1;
+    //  Filedescriptor(Connection-Descriptor)
+    int cnnct_fd = -1;
+    //  Integer for return value of bind
+    int bind_result = -1;
+    //  Integer for return value of listen
+    int lstn = -1;
+    // integer for return value of read and number of transmitted bytes/characters
+    int val_read = -1;
+    // integer for return value of send
+    int send_result = -1;
+    //  Number of bytes the client has send
+    int bytes_read;
 
-    struct sockaddr_in client; // Socketadresse eines Clients
-    socklen_t client_len; // Länge der Client-Daten
-    char in[BUFSIZE]; // Daten vom Client an den Server
-    int bytes_read; // Anzahl der Bytes, die der Client geschickt hat
+    //  String buffer to send messages between server and client
+    char buffer[LENGTH];
+    // Strings to send messages to the client
+    char answer[LENGTH][LENGTH] = {"Halli hallo!", "Muss muss und dir?" , "Sehr schön, man sieht sich!" , "Darauf antworte ich nicht."};
+    // Selector int for answer
+    int answr;
 
+    //TODO Check whats going on here
+    memset(buffer, 0, sizeof(buffer));
 
-    // Socket erstellen
-    rfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (rfd < 0 ){
-        fprintf(stderr, "socket konnte nicht erstellt werden\n");
-        exit(-1);
+    /*  Creating the socket with ipv4 and tcp no further description for protocol needed so 0 is enough -> SOCK_STREAM uses tcp by default.
+    *   AF_INET = ipv4
+    *   SOCK_STREAM = tcp
+    *   protocol 0 = default (For other protocols to expect when working with ip)
+    */
+    rndzvs_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //  Return value of rndzvs_fd is negative when an error occurs. And 0 when successfull. If it wasn't successfull terminate program
+    if (rndzvs_fd <0){
+        perror("Error while socket()");
+        return EXIT_FAILURE;
     }
 
+    //  Define to use ipv4
+    address.sin_family = AF_INET;
+    //  Define from which ip-address clients want to connect to server, any adress is allowed
+    address.sin_addr.s_addr = INADDR_ANY;
+    //  Define port number
+    address.sin_port = htons(PORT_NUMBER);
 
-    // Socket Optionen setzen für schnelles wiederholtes Binden der Adresse
-    int option = 1;
-    setsockopt(rfd, SOL_SOCKET, SO_REUSEADDR, (const void *) &option, sizeof(int));
+    /*  Bind an address to the socket
+     *  rndzvs_fd = socket file descriptor
+     *  addr = address struct for cast
+     *  len = length of the adress
+     */
+    bind_result = bind(rndzvs_fd, (struct sockaddr *) &address, sizeof(address));
+    // Ask for return value of the function to check for error while binding => terminate program if error
+    if(bind_result < 0){
+        perror("Error while bind()");
+        return EXIT_FAILURE;
+    }
+//  Max. capacity for clients communicating with the server
+    const int max_clients = 5;
 
-
-    // Socket binden
-    struct sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(PORT);
-    int brt = bind(rfd, (struct sockaddr *) &server, sizeof(server));
-    if (brt < 0 ){
-        fprintf(stderr, "socket konnte nicht gebunden werden\n");
-        exit(-1);
+    /*  Bring socket to listen for incoming connections
+     *  rndzvs_fd = socket file descriptor
+     *  n = maximum number of clients
+     *
+     */
+    lstn = listen(rndzvs_fd, max_clients);
+    // Check return value for error
+    if (lstn < 0 ){
+        perror("Error while listen()");
+        return EXIT_FAILURE;
     }
 
+    // continuous while loop for server to maintain its service until disruption with "str/ctrl + c"
+    while(LOOP){
 
-    // Socket lauschen lassen
-    int lrt = listen(rfd, 5);
-    if (lrt < 0 ){
-        fprintf(stderr, "socket konnte nicht listen gesetzt werden\n");
-        exit(-1);
-    }
+        /*  Accept incoming connection request
+         *
+         *  cnnct_fd = socket file descriptor
+         *  addr = source adress
+         *  addr_len = size of source adress
+         */
+        //TODO Nochmal alle sockets überprüfen höchstwahrscheinlich sind hier Dreher drin
+        rndzvs_fd = accept(rndzvs_fd, (struct sockaddr *) &address, (socklen_t *) &addr_length);
 
-    while (ENDLOSSCHLEIFE) {
+        if(rndzvs_fd < 0) {
+            perror("Error while accept()");
+            return EXIT_FAILURE;
+        }
 
-        // Verbindung eines Clients wird entgegengenommen
-        cfd = accept(rfd, (struct sockaddr *) &client, &client_len);
+        /*  cnnct_fd = connect file descriptor
+         *  buf = text buffer
+         *  nbytes = size of text buffer
+         *
+         */
+        val_read = read(cnnct_fd, buffer, LENGTH);
+        if(val_read < 0){
+            perror("Error while read()");
+            return EXIT_FAILURE;
+        }
 
-        // Lesen von Daten, die der Client schickt
-        bytes_read = read(cfd, in, BUFSIZE);
+        // Store transmitted message, in a string, to stdout
+        puts(buffer);
 
-        // Zurückschicken der Daten, solange der Client welche schickt (und kein Fehler passiert)
-        while (bytes_read > 0) {
-            printf("sending back the %d bytes I received...\n", bytes_read);
+        // evaluate answer
+        if(buffer == "Hallo."){
+            answr = 0;
+        }
+        else if(buffer == "Wie geht's?"){
+            answr = 1;
 
-            write(cfd, in, bytes_read);
-            bytes_read = read(cfd, in, BUFSIZE);
+        }else if(buffer == "Joa, muss."){
+            answr = 2;
 
         }
-        close(cfd);
+        else{
+            answr = 3;
+
+        }
+
+        /*  Send answer to client via tcp (send requires tcp)
+         *  cnnct_fd = socket file descriptor
+         *  buf = text buffer
+         *  n = length of the buffer
+         *  flags = set if required, default value is 0 -> man send
+         */
+        send(cnnct_fd, answer[answr], strlen(answer[answr]),0);
+        if(send_result < 0){
+            perror("Error while send()");
+            return EXIT_FAILURE;
+        }
+
+        // TODO
+        memset(buffer, 0, sizeof(buffer));
+
+        if(close(rndzvs_fd) < 0){
+            perror("Error while close()");
+            return EXIT_FAILURE;
+        }
     }
 
-    // Rendevouz Descriptor schließen
-    close(rfd);
+
+    return EXIT_SUCCESS;
+
 
 }
